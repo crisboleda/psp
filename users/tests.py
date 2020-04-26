f42d3c9fe46efb23afe3d40c9b0ad5bee9920297
bycristhian <cristhian.2002.0@gmail.com>
2020-04-25 20:00:12 -0500

    Se crea la lógica de la autenticación con email o username y sus respectivas pruebas unitarias

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
 
-# Create your tests here.
+# Django
+from django.test import TestCase
+from django.contrib.auth import authenticate
+
+# Models
+from django.contrib.auth.models import User
+
+
+class UserAuthenticateTest(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create_user(username='cristhian', email='cristhian@gmail.com', password='123')
+        self.user2 = User.objects.create_user(username='laura', email='laura@gmail.com', password='lol')
+
+    def test_authenticate_user_username(self):
+        username = 'cristhian'
+        password = '123'
+
+        user = authenticate(username=username, password=password)
+
+        self.assertEquals(user, self.user1)
+
+
+    def test_authenticate_user_email(self):
+        email = 'laura@gmail.com'
+        password = 'lol'
+
+        user = authenticate(username=email, password=password)
+
+        self.assertEquals(user, self.user2)
