af501587369ba72153ceb9319aa470a96f08d068
bycristhian <cristhian.2002.0@gmail.com>
2020-04-28 19:42:32 -0500

    Refactorización y documentación de los test de autenticación

--- users/tests.py
+++ users/tests.py
@@ -13,19 +13,21 @@ class UserAuthenticateTest(TestCase):
         self.user1 = User.objects.create_user(username='cristhian', email='cristhian@gmail.com', password='123')
         self.user2 = User.objects.create_user(username='laura', email='laura@gmail.com', password='lol')
 
+        self.username_user1 = 'cristhian'
+        self.password_user1 = '123'
+
+        self.email_user2 = 'laura@gmail.com'
+        self.password_user2 = 'lol'
+
+    # Test authentication with username and password
     def test_authenticate_user_username(self):
-        username = 'cristhian'
-        password = '123'
 
-        user = authenticate(username=username, password=password)
-
+        user = authenticate(username=self.username_user1, password=self.password_user1)
         self.assertEquals(user, self.user1)
 
 
+    # Test authentication with email and password
     def test_authenticate_user_email(self):
-        email = 'laura@gmail.com'
-        password = 'lol'
 
-        user = authenticate(username=email, password=password)
-
+        user = authenticate(username=self.email_user2, password=self.password_user2)
         self.assertEquals(user, self.user2)
