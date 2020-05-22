a5a7b719d9edb411ecc45916abca8e51cd4f1de4
bycristhian <cristhian.2002.0@gmail.com>
2020-05-22 02:04:09 -0500

    Solucionando errores y mejorando funcionalidad

--- logs/templatetags/convert_time.py
+++ logs/templatetags/convert_time.py
@@ -5,7 +5,7 @@ from django import template
 register = template.Library()
 
 @register.filter
-def seconds_to_time(value):
+def seconds_to_time(value) -> str:
     hours = int(value / 3600)
     minutes = int((value % 3600) / 60)
     seconds = int((value % 3600) % 60)
