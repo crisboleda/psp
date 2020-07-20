a077e05c1a52aea30d458087141e63c7579e657b
bycristhian <cristhian.2002.0@gmail.com>
2020-07-20 01:02:30 -0500

    Se implementan más pruebas unitarias de distintos

--- logs/forms.py
+++ logs/forms.py
@@ -15,9 +15,9 @@ class CreateLogProgramForm(forms.Form):
         try:
             self.phase = Phase.objects.get(name=self.cleaned_data['name_phase'])
         except Phase.DoesNotExist:
-            forms.ValidationError("The phase doesn't exists")
+            raise forms.ValidationError("The phase doesn't exists")
 
-    
+
     def save(self, program):
         data = self.cleaned_data
         TimeLog.objects.create(phase=self.phase, program=program, comments=data['comments'])
