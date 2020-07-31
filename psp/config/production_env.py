9f689554061f08712f861618606b2060da457041
bycristhian <cristhian.2002.0@gmail.com>
2020-07-31 00:07:12 -0500

    Se configura las variables de envio de correos en producción

--- psp/config/production_env.py
+++ psp/config/production_env.py
@@ -50,8 +50,6 @@ BASE_URL = "https://psp-adsi.site"
 SECURE_SSL_REDIRECT = True
 
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django_mailjet.backends.MailjetBackend'
+MAILJET_API_KEY = config('MAILJET_API_KEY')
+MAILJET_API_SECRET = config('MAILJET_API_SECRET')
